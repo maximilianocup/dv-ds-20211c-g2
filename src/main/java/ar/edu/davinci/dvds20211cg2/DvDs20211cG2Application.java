c823c3e8b35ef1cb17b7634ad429bb12f4543815
Brandalise <jb250854@ncr.com>
2021-07-12 19:24:20 -0300

    commit inicial

---
+++ src/main/java/ar/edu/davinci/dvds20211cg2/DvDs20211cG2Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20211cg2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20211cG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20211cG2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20211cg2/DvDs20211cG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20211cg2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20211cG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
